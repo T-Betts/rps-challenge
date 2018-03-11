de1197c9bafd1e68d730fb5522f4de49c9c4bcb3
Tom Betts <tom.betts@live.co.uk>
2018-03-11 12:22:13 +0000

    pass name feature test and style '/' route in index.erb

--- app.rb
+++ app.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+
   get '/' do
-    "Welcome to RPS "
+    erb(:index)
+  end
+
+  post '/play' do
+    'Hey Tom! Welcome to RPS'
   end
   run! if app_file == $0
 end
--- spec/features/entering_player_name_spec.rb
+++ spec/features/entering_player_name_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 feature 'Entering player name' do
   scenario 'Can enter player name and see them on screen' do
     visit('/')
-    expect(page).to have_content('Welcome to RPS')
+    fill_in('Player_1', with: 'Tom')
+    click_button('Start')
+    expect(page).to have_content('Hey Tom! Welcome to RPS')
   end
 end
