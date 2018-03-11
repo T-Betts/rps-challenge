fea3510296f4d71ca6a33e332cb7bad481b96e0a
Tom Betts <tom.betts@live.co.uk>
2018-03-11 13:04:35 +0000

    enable sessions

--- app.rb
+++ app.rb
@@ -1,13 +1,21 @@
 require 'sinatra/base'
 
 class RockPaperScissors < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    'Hey Tom! Welcome to RPS'
+  post '/name' do
+    session[:player_name] = params[:Player_name]
+    redirect('play')
   end
+
+  get '/play' do
+    @player = session[:player_name]
+    "Hey #{@player}! Welcome to RPS"
+  end
+
   run! if app_file == $0
 end
